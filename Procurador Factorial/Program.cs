a18547923023c435697d7ff20cabe50c3c6e457e
GuiBelchior <137189300+GuiBelchior@users.noreply.github.com>
2023-10-27 11:10:31 +0100

    Add files via upload

---
+++ Procurador Factorial/Program.cs
@@ -0,0 +1,57 @@
+﻿//Há a necessidade de criar uma aplicação que, perante a introdução de dois
+//limites inteiros, calcule a quantidade e o somatório de todos os valores ímpares
+//nesse intervalo. Note que os limites do intervalo podem ser inseridos no menor para
+//o maior ou do maior para o menor.
+//• Desenhe e implemente um algoritmo que resolva o problema usando cada um
+//dos ciclos que aprendeu..
+
+//variantes
+double primeiroNumero;
+double ultimoNumero;
+double numeroAuxiliar;
+double contadorNumeros = 0;
+string listaPar = "";
+string listaImpar = "";
+double contadorNumerosPares = 0;
+double contadorNumerosImpares= 0;
+//Pedir os intervalo de numeros
+Console.WriteLine("Indique o primeiro numero");
+primeiroNumero=double.Parse(Console.ReadLine());
+
+Console.WriteLine("Indique o segundo numero");
+ultimoNumero= double.Parse(Console.ReadLine());
+
+// Aplicar Teoria dos Canecos em caso de erro do utilizador
+if (primeiroNumero > ultimoNumero)
+{
+    numeroAuxiliar = primeiroNumero;
+    primeiroNumero = ultimoNumero;
+    ultimoNumero = numeroAuxiliar;
+}
+contadorNumeros = 0 + primeiroNumero;
+
+//procura pelos numeros os numeros pares e impares
+while (contadorNumeros <= ultimoNumero)
+{
+    if (contadorNumeros % 2 == 0)
+    {
+        listaPar += $"{contadorNumeros}";
+        listaPar += ";";
+        Console.WriteLine("este numero é par");
+        contadorNumerosPares++;
+    }
+    else
+    {
+        listaImpar += $"{contadorNumeros}";
+        listaImpar += ";";
+        Console.WriteLine("este numero é impar");
+        contadorNumerosImpares++;
+    }
+    contadorNumeros++;
+}
+//Implementar um resultado que diga Numero de impares entre X e Y, que são os : xxxxxxx;
+Console.WriteLine($"De os numeros {primeiroNumero} e {ultimoNumero} foi identificados: {contadorNumerosPares} numeros pares sendo eles {listaPar}");
+Console.WriteLine($"De os numeros {primeiroNumero} e {ultimoNumero} foi identificados: {contadorNumerosImpares} numeros pares sendo eles {listaImpar}");
+
+
+
---
+++ os números múltiplos de 3 entre xy/Program.cs
@@ -0,0 +1,63 @@
+﻿//Escreva na consola todos os números múltiplos de 3 entre [x;y];
+//CONSTANTES
+const int DIVISOR = 3;
+
+//Variaveis
+int auxiliar;
+int segundoNumero;
+int inicialNumero;
+string multiplos = "";
+string naoMultiplos = "";
+
+//Perguntar pelo primeiro numero
+Console.WriteLine("Qual é o seu primeiro numero?");
+inicialNumero = int.Parse(Console.ReadLine());
+// Perguntar pelo segundo numero
+Console.WriteLine("Qual é o seu segundo numero?");
+segundoNumero = int.Parse(Console.ReadLine());
+
+//trocar os valores se necessario
+if (inicialNumero > segundoNumero)
+{
+    auxiliar = inicialNumero;
+    inicialNumero = segundoNumero;
+    segundoNumero= auxiliar;    
+}
+
+// Confirmar as escolhas
+Console.WriteLine($"Multiplos de {DIVISOR} de {inicialNumero} e {segundoNumero}");
+
+//Encontrar os multiplos de 3 entre os dois numeros
+while (inicialNumero <= segundoNumero) //Aqui o while vai ter uma formula if dentro do seu ciclo, cada vez que passa por um numero verifica se é multiplo 
+{
+    if (inicialNumero % DIVISOR == 0)
+    {
+        multiplos += inicialNumero;
+        multiplos += ";";
+    }
+    else
+    {
+        naoMultiplos += inicialNumero;
+        naoMultiplos += ";";
+    }
+    inicialNumero++;
+}
+
+//Resultado
+if (multiplos != "")
+{
+    Console.WriteLine($"Os multiplos de {DIVISOR} são:{multiplos}");
+}
+else 
+{
+    Console.WriteLine("Não existem multiplos.");
+}
+
+if (naoMultiplos != "")
+{
+    Console.WriteLine($"Os nao multiplos de {DIVISOR} são:{naoMultiplos}");
+}
+else
+{
+    Console.WriteLine("Nao existem nao multiplos.");
+}
